d2a49dbecc846da16a1776b410f6461e7278df68
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2019-06-27 12:31:22 -0300

    tela de cadastro de usuário

--- src/app/usuarios/model.ts
+++ src/app/usuarios/model.ts
@@ -1,4 +1,6 @@
 export class Usuario {
   id: number;
   nome: string;
+  sexo: string;
+  idade: number;
 }
--- src/app/usuarios/usuarios-cadastro/usuarios-cadastro.component.ts
+++ src/app/usuarios/usuarios-cadastro/usuarios-cadastro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model';
+import { FormControl } from '@angular/forms';
+import { UsuariosService } from '../usuarios.service';
+import { MessageService } from 'primeng/api';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuarios-cadastro',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios-cadastro.component.css']
 })
 export class UsuariosCadastroComponent implements OnInit {
+  usuario = new Usuario();
 
-  constructor() { }
+  constructor(
+    private usuariosService: UsuariosService,
+    private messageService: MessageService,
+    private rota: ActivatedRoute
+  ) {
+
+  }
 
   ngOnInit() {
+    const { id } = this.rota.snapshot.params;
+    if (id) {
+      this.carregarusuario(id);
+    }
+  }
+
+  async carregarusuario(id:number){
+    this.usuario = await this.usuariosService.buscarPorCodigo(id);
+  }
+
+  async alterar(form: FormControl) {
+    try {
+      await this.usuariosService.alterar(this.usuario)
+
+      this.messageService.add({
+        severity:'success',
+        summary:'Edição',
+        detail:'Usuário ' + this.usuario.nome + ' alterado'
+      });
+
+      form.reset();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  salvar(form: FormControl) {
+    if (this.editando) {
+      return this.alterar(form);
+    }
+
+    return this.inserir(form);
+  }
+
+  async inserir(form: FormControl) {
+    await this.usuariosService.adicionar(this.usuario)
+
+    this.messageService.add({
+      severity:'success',
+      summary:'Cadastro',
+      detail:'Usuário ' + this.usuario.nome + ' cadastrado'
+    });
+
+    form.reset();
+  }
+
+  get editando(){
+    return Boolean(this.usuario.id);
   }
 
 }
